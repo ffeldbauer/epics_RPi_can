2f791bd4b3f301f185c4dfa6dae11bf0b75cb87f
Florian Feldbauer <florian@ep1.ruhr-uni-bochum.de>
2013-05-23 15:14:54 +0200

    updated readme

--- CAN_interface/driver/can_fifo.c
+++ CAN_interface/driver/can_fifo.c
@@ -1,12 +1,30 @@
 //******************************************************************************
-//! @file    can_fifo.c
-//!
-//! @author  F. Feldbauer
-//!
-//! @brief   all about fifo buffer management
-//!          BASED ON peak-linux-driver-7.6 (http://www.peak-system.com/)
-//!
-//! @version 2.0.0
+// Copyright (C) 2012 Florian Feldbauer <florian@ep1.ruhr-uni-bochum.de>
+//                    - Ruhr-Universitaet Bochum, Lehrstuhl fuer Experimentalphysik I
+//
+// This file is part of rpi_can kernel module
+//
+// rpi_can is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// rpi_can is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// This source code is based on peak-linux-driver-7.6
+//   linux@peak-system.com
+//   www.peak-system.com
+//
+// brief   all about fifo buffer management
+//
+// version 2.0.0
 //******************************************************************************
 
 //_____ I N C L U D E S _______________________________________________________
--- CAN_interface/driver/can_filter.h
+++ CAN_interface/driver/can_filter.h
@@ -1,29 +1,30 @@
 //******************************************************************************
-//! Copyright (C) 2012 Florian Feldbauer
-//!
-//! This program is free software; you can redistribute it and/or modify
-//! it under the terms of the GNU General Public License as published by
-//! the Free Software Foundation; either version 3 of the License, or
-//! (at your option) any later version.
-//!
-//! This program is distributed in the hope that it will be useful,
-//! but WITHOUT ANY WARRANTY; without even the implied warranty of
-//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//! GNU General Public License for more details.
-//!
-//! You should have received a copy of the GNU General Public License
-//! along with this program; if not, write to the Free Software
-//! Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//!
-//! This source code is based on peak-linux-driver-7.6
-//!   linux@peak-system.com
-//!   www.peak-system.com
-//!
-//! @author  F. Feldbauer <florian@ep1.ruhr-uni-bochum.de>
-//!
-//! @brief   all about CAN message filtering
-//!
-//! @version 2.0.0
+// Copyright (C) 2012 Florian Feldbauer <florian@ep1.ruhr-uni-bochum.de>
+//                    - Ruhr-Universitaet Bochum, Lehrstuhl fuer Experimentalphysik I
+//
+// This file is part of rpi_can kernel module
+//
+// rpi_can is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// rpi_can is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// This source code is based on peak-linux-driver-7.6
+//   linux@peak-system.com
+//   www.peak-system.com
+//
+// brief   all about CAN message filtering
+//
+// version 2.0.0
 //******************************************************************************
 
 #ifndef __CAN_FILTER_H__
